366013f44d2d680fa71fc5bc3777a4165fa5a544
Luck <git@lucko.me>
2025-08-25 10:25:09 +0100

    Update default API URL

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,5 +1,5 @@
 export const bytebinUrl =
-  import.meta.env.VITE_BYTEBIN_URL || 'https://bytebin.lucko.me/';
+  import.meta.env.VITE_BYTEBIN_URL || 'https://api.pastes.dev/';
 export const postUrl = bytebinUrl + 'post';
 export const useQueryRouting = 
   import.meta.env.VITE_USE_QUERY_ROUTING === 'true';
